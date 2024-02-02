69d137fcee602fd9632fdac6e564e72247ea616a
camilajullyane <camilaj.silva0@gmail.com>
2024-02-02 18:58:14 -0300

    uhu

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from time import sleep
+
+url = 'http://transito.gtrans.com.br/cttupe/index.php/portal/getImg/192.168.10.105/'
+
+
+count = 0
+while True:
+    response = requests.get(url)
+    with open(f'imagem_{count}.png', 'wb') as file:
+        file.write(response.content)
+    print(f'imagem_{count}.png criada')
+    count += 1
+    sleep(180)
