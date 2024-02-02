9994d8f111fea2ee9be1a0133e0c1ec9fa224109
Victor Salgado <joaosalgado605@gmail.com>
2024-02-02 19:52:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from time import sleep
 
 url = 'http://transito.gtrans.com.br/cttupe/index.php/portal/getImg/192.168.10.105/'
 
-
 count = 15
-while True:
-    response = requests.get(url)
-    with open(f'imagem_{count}.png', 'wb') as file:
-        file.write(response.content)
-    print(f'imagem_{count}.png criada')
-    count += 1
-    sleep(180)
+
+response = requests.get(url)
+with open(f'imagem_{count}.png', 'wb') as file:
+    file.write(response.content)
+print(f'imagem_{count}.png criada')
+count += 1
